20c5f0c4d6b299558e91d8baf2392c36994bd880
Hmlwh4 <hayman6104@gmail.com>
2023-03-03 13:28:14 +0630

    just test

---
+++ no-aop/src/main/java/com/demo/ds/Report.java
@@ -0,0 +1,4 @@
+package com.demo.ds;
+
+public class Report {
+}
---
+++ no-aop/src/main/java/com/demo/service/ComplexAction.java
@@ -0,0 +1,42 @@
+package com.demo.service;
+
+import com.demo.dao.ReportDao;
+import com.demo.ds.Report;
+import com.demo.format.ReportFormatter;
+import com.demo.logger.PerformanceLogger;
+import com.demo.repository.ReportProvider;
+import org.springframework.stereotype.Service;
+import static com.demo.logger.PerformanceLogger.*;
+
+@Service
+public class ComplexAction {
+
+    private ReportDao reportDao;
+    private ReportFormatter reportFormatter;
+    private ReportProvider reportProvider;
+
+    public ComplexAction(ReportDao reportDao, ReportFormatter reportFormatter, ReportProvider reportProvider) {
+        this.reportDao = reportDao;
+        this.reportFormatter = reportFormatter;
+        this.reportProvider = reportProvider;
+    }
+
+    public void takeAction(){
+        PerformanceLogger logger = new PerformanceLogger();
+        PerformanceLogger.PerformanceInfo performanceInfo = logger
+                .begin("ReportFormatter::formatReport(new Report())");
+        Report formattedReport = reportFormatter.formatReport(new Report()); //primary concern
+        logger.end(performanceInfo);
+
+        performanceInfo=logger
+                .begin("ReportDao::save(formattedReport)");
+        reportDao.save(formattedReport);
+        logger.end(performanceInfo);
+
+        performanceInfo = logger
+                .begin("ReportProvider::reportProvider.reportProvider()");
+        Report report = reportProvider.reportProvide();
+        logger.end(performanceInfo);
+    }
+}
+
